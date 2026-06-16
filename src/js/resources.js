167831cfb1ad2bcf26d8215ee3bad2948e6053d9
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-06-16 13:33:09 +0200

    added back and front animations for the player and added the bedroom(empty) scene

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,7 +3,7 @@ import { ImageSource, Sound, Resource, Loader } from "excalibur";
 // voeg hier jouw eigen resources toe
 const Resources = {
   BackgroundTopDown: new ImageSource("images/background-topdown.png"),
-  Bedroom: new ImageSource("images/background-classroom.png"),
+  Bedroom: new ImageSource("images/BedRoomMC.png"),
   PlayerSheet: new ImageSource("images/playersheet.png"),
   Textbox: new ImageSource("images/textbox.png"),
 };
