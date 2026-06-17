65e5f3207496b2d0d4d4e51ada165e6082a09ac4
Hollanderfloris <hollanderfloris@gmail.com>
2026-06-17 15:40:17 +0200

    Added limits to the character creation so it cannot go into the negative or past the available choices, doesn't work as well but bettter than previously.

--- src/js/classes/scenes/characterCreation.js
+++ src/js/classes/scenes/characterCreation.js
@@ -2,9 +2,6 @@ import { Actor, Engine, Scene, ScreenElement, Vector } from "excalibur";
 import { Resources } from "../../resources.js";
 import { Background } from "../gameobjects/background.js";
 import { Player } from "../gameobjects/player.js";
-import { Bedroom } from "./bedroom.js";
-import { Shirt } from "../gameobjects/shirt.js"
-import { Pants } from "../gameobjects/pants.js";
 import { ArrowLeft } from "../gameobjects/arrowLeft.js";
 import { ArrowRight } from "../gameobjects/arrowRight.js";
 
@@ -32,14 +29,59 @@ export class CharacterCreation extends Scene {
 
     updateCharacter(bodypart, direction) {
         // Updates the bodyparts array 
-        // console.log(`bodypart ${bodypart} moet richting ${direction}`)
         this.engine.bodyparts[bodypart] = this.engine.bodyparts[bodypart] + direction
-        this.player.updateGraphics()
-        if (this.engine.bodyparts > [1, 3, 3, 2, 1] || this.engine.bodyparts < [0, 0, 0, 0, 0]) {
 
-            this.engine.bodyparts = [0, 0, 0, 0, 0]
-            console.log(`I reset the body parts`)
+        // Ensures that the array cannot go into the negative numbers
+        for (let i = 0; i < 5; i++) {
+
+            if (this.engine.bodyparts[i] < 0) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the hair`)
+            }
+            if (this.engine.bodyparts[i] < 0) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the skin color`)
+            }
+            if (this.engine.bodyparts[i] < 0) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the shirt`)
+            }
+            if (this.engine.bodyparts[i] < 0) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the pants`)
+            }
+            if (this.engine.bodyparts[i] < 0) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the shoes`)
+            }
         }
+        // Ensures that the array cannot go past the number of available clothes.
+        for (let i = 0; i < 5; i++) {
+
+            if (this.engine.bodyparts[i] > 3) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the hair`)
+            }
+            if (this.engine.bodyparts[i] > 3) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the skin color`)
+            }
+            if (this.engine.bodyparts[i] > 3) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the shirt`)
+            }
+            if (this.engine.bodyparts[i] > 3) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the pants`)
+            }
+            if (this.engine.bodyparts[i] > 3) {
+                this.engine.bodyparts[i] = 0
+                console.log(`I reset the shoes`)
+            }
+        }
+
+
+        this.player.updateGraphics()
     }
 
 
