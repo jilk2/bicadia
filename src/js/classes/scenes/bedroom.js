a2274dcfddfd8a08880e944fb13c8a54ba8af5a1
SkAnnika <korstenannika@gmail.com>
2026-06-17 12:23:25 +0200

    invisble wall created

--- src/js/classes/scenes/bedroom.js
+++ src/js/classes/scenes/bedroom.js
@@ -1,6 +1,7 @@
 import { Actor, Engine, Scene} from "excalibur";
 import { Resources } from "../../resources.js";
 import { Background } from "../gameobjects/background.js";
+import { InvisibleWall } from "../gameobjects/invisible-wall.js"
 import { Player } from "../gameobjects/player.js";
 import { Wardrobe } from "../gameobjects/wardrobe.js";
 import { Bed } from "../gameobjects/bed.js";
@@ -11,6 +12,10 @@ export class Bedroom extends Scene {
   onInitialize(engine) {
     const background = new Background(Resources.Bedroom);
     this.add(background);
+
+    //borders
+    this.add(new InvisibleWall(0, 300, 10, 1000))      // links
+
 
     this.add(new Wardrobe(1000, 200))
     this.add(new Bed(715, 300))
