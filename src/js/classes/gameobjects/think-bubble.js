af887268325a00b3cd547769650d3292f1a00723
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-06-18 13:54:48 +0200

    added thinkBubble for the goal for the player

---
+++ src/js/classes/gameobjects/think-bubble.js
@@ -0,0 +1,26 @@
+import { Actor, Color, Label, Vector, TextAlign, Font } from "excalibur";
+import { Resources } from "../../resources";
+
+export class ThinkBubble extends Actor {
+  constructor(initialText = "") {
+    super({ x: 1150, y: 50 });
+    this.sprite = Resources.thinkBubble.toSprite();
+    this.graphics.use(this.sprite);
+    this.scale = new Vector(1, 1);
+    this.graphics.flipHorizontal = true;
+
+    this.textLabel = new Label({
+      color: Color.Black,
+      x: -80,
+      y: -15,
+      text: "i need to find my backpack",
+      scale: new Vector(1.3,1.5)
+      // scale: new Vector(-5, -5),
+    });
+    this.addChild(this.textLabel);
+  }
+
+  loadNextGoal(dialogue) {
+    this.textLabel.text = `${dialogue}`;
+  }
+}
--- src/js/classes/scenes/classroom.js
+++ src/js/classes/scenes/classroom.js
@@ -1,30 +1,32 @@
 import { Actor, Engine, Scene } from "excalibur";
 import { Resources } from "../../resources.js";
 import { Background } from "../gameobjects/background.js";
-import { InvisibleWall } from "../gameobjects/invisible-wall.js"
+import { InvisibleWall } from "../gameobjects/invisible-wall.js";
 import { Player } from "../gameobjects/player.js";
-import { Door } from "../gameobjects/door.js"
+import { Door } from "../gameobjects/door.js";
 import { InteractableNpc } from "../gameobjects/interactable-npc.js";
+import { ThinkBubble } from "../gameobjects/think-bubble.js";
 
 export class Classroom extends Scene {
-    onInitialize(engine) {
-        const background = new Background(Resources.Classroom);
-        this.add(background);
+  onInitialize(engine) {
+    const background = new Background(Resources.Classroom);
+    this.add(background);
 
-        //borders
-        this.add(new InvisibleWall(0, 300, 10, 1000))         // links
-        this.add(new InvisibleWall(1280, 300, 10, 1000))      // rechts
-        this.add(new InvisibleWall(1280, 240, 150, 40))      // boven (rechts van deur)
-        this.add(new InvisibleWall(0, 240, 2187, 40))          // boven (links van deur)
-        this.add(new InvisibleWall(800, 730, 1900, 40))      // onder
+    //borders
+    this.add(new InvisibleWall(0, 300, 10, 1000)); // links
+    this.add(new InvisibleWall(1280, 300, 10, 1000)); // rechts
+    this.add(new InvisibleWall(1280, 240, 150, 40)); // boven (rechts van deur)
+    this.add(new InvisibleWall(0, 240, 2187, 40)); // boven (links van deur)
+    this.add(new InvisibleWall(800, 730, 1900, 40)); // onder
 
+    this.add(new Door(1150, 180, "bedroom"));
+    this.thinkBubble = new ThinkBubble()
+    this.add(this.thinkBubble);
+    this.thinkBubble.loadNextGoal("I need to ask someone for a pen");
 
+    this.player = new Player(690, 360);
+    this.add(this.player);
 
-        this.add(new Door(1150, 180, "bedroom"))
-
-        this.player = new Player(690, 360)
-        this.add(this.player)
-
-        this.add(new InteractableNpc(600, 400))
-    }
+    this.add(new InteractableNpc(600, 400));
+  }
 }
