1e68952fe9ccdc99a10ac27ca7e200611b3db061
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-06-18 11:17:06 +0200

    when the player needs to go to school, he can interact with the door to go to school

--- src/js/classes/gameobjects/backpack.js
+++ src/js/classes/gameobjects/backpack.js
@@ -1,24 +1,28 @@
-import { CollisionType, Actor, Vector } from 'excalibur'
+import { CollisionType, Actor, Vector } from "excalibur";
 import { Resources } from "../../resources";
-import { Player } from './player';
+import { Player } from "./player";
+import { DialogueHandler } from "../dialogueHandler";
 
 export class Backpack extends Actor {
+  constructor(x, y) {
+    super({
+      x,
+      y,
+      width: Resources.Backpack.width,
+      height: Resources.Backpack.height,
+    }); // collision box!
+    this.pos = new Vector(x, y);
+    this.interactable = true;
+  }
 
-    constructor(x, y) {
-        super({ x, y, width: Resources.Backpack.width, height: Resources.Backpack.height }) // collision box! 
-        this.pos = new Vector(x,y)
-        this.interactable = true
+  onInitialize(engine) {
+    this.graphics.use(Resources.Backpack.toSprite());
+    this.body.collisionType = CollisionType.Fixed;
+    this.scale = new Vector(0.125, 0.125);
+  }
 
-    }
-    
-    onInitialize(engine) {
-        this.graphics.use(Resources.Backpack.toSprite())
-        this.body.collisionType = CollisionType.Fixed
-        this.scale = new Vector(0.125 , 0.125)
-    }
-
-    interaction(engine){
-        this.kill()
-    }
-    
+  interaction(engine) {
+    this.kill();
+    DialogueHandler.activateHomeEndDialogue(engine, "backpackDialogueEnd");
+  }
 }
