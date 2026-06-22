4b6913ff0700b4fb1366065f256fdf463434f444
Finn van Broekhoven <finnbroekhoven@gmail.com>
2026-06-22 11:08:53 +0200

    Desk and chair added now fixing them

--- src/js/classes/scenes/bedroom.js
+++ src/js/classes/scenes/bedroom.js
@@ -1,4 +1,4 @@
-import { Actor, Engine, Scene} from "excalibur";
+import { Actor, Engine, Scene } from "excalibur";
 import { Resources } from "../../resources.js";
 import { Background } from "../gameobjects/background.js";
 import { InvisibleWall } from "../gameobjects/invisible-wall.js"
@@ -10,6 +10,8 @@ import { Door } from "../gameobjects/door.js"
 import { Plant } from "../gameobjects/plant.js";
 import { Livingroom } from "./livingroom.js";
 import { ThinkBubble } from "../gameobjects/think-bubble.js";
+import { Desk } from "../gameobjects/desk.js";
+import { Deskchair, DeskChair } from "../gameobjects/deskchair.js";
 
 export class Bedroom extends Scene {
   onInitialize(engine) {
@@ -22,16 +24,18 @@ export class Bedroom extends Scene {
     this.add(new InvisibleWall(1030, 220, 1020, 100))      // boven (rechts van deur)
     this.add(new InvisibleWall(0, 220, 840, 100))          // boven (rechts van deur)
     this.add(new InvisibleWall(800, 740, 1900, 80))      // onder
-    
+
     //decoration
     this.add(new Wardrobe(1000, 200))
     this.add(new Bed(715, 300))
     this.add(new Backpack(500, 500))
     this.add(new Door(470, 180, "livingroom"))
     this.add(new Plant(1075, 120))
+    this.add(new Desk(1230, 605))
+    this.add(new Deskchair(1210, 630))
     this.thinkBubble = new ThinkBubble()
     this.add(this.thinkBubble)
-    
+
     this.player = new Player(1000, 360)
     this.add(this.player)
 
