929f2bb1de36fa31493f0d66f02d0140a2124c32
jilk2 <105498943+jilk2@users.noreply.github.com>
2026-06-22 14:14:01 +0200

    new think-bubble.js

--- src/js/classes/gameobjects/think-bubble.js
+++ src/js/classes/gameobjects/think-bubble.js
@@ -3,18 +3,18 @@ import { Resources } from "../../resources";
 
 export class ThinkBubble extends Actor {
   constructor(initialText = "") {
-    super({ x: 1150, y: 50 });
+    super({ x: 1200, y: 50 });
     this.sprite = Resources.thinkBubble.toSprite();
     this.graphics.use(this.sprite);
-    this.scale = new Vector(1, 1);
-    this.graphics.flipHorizontal = true;
+    this.scale = new Vector(0.2, 0.2);
+    // this.graphics.flipHorizontal = true;
 
     this.textLabel = new Label({
       color: Color.Black,
-      x: -80,
+      x: -480,
       y: -15,
       text: "i need to find my backpack",
-      scale: new Vector(1.3, 1.5)
+      scale: new Vector(7, 7)
       // scale: new Vector(-5, -5),
     });
     this.addChild(this.textLabel);
